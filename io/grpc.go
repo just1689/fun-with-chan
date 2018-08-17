9b593ad7333c328b64399ae6239cc53b3df64162
Captain Justin <Just1689@gmail.com>
2018-08-17 23:30:08 +0200

    Services have greater scope than operation

--- io/grpc.go
+++ io/grpc.go
@@ -5,6 +5,7 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"net"
+	"log"
 )
 
 type SubsciberServer struct {
@@ -21,7 +22,10 @@ func StartServer() {
 	server := grpc.NewServer()
 	var subscribers SubsciberServer
 	fun.RegisterSubscribeServer(server, subscribers)
-	li, _ := net.Listen("tcp", ":8000")
+	li, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		log.Fatal("Could not GRPC start server")
+	}
 	server.Serve(li)
 
 }
